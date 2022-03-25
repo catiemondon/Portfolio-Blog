4c098a782e49c7594acffddab0cd9a04df02c5da
Cate Mondon <cathmond25@gmail.com>
2022-03-25 17:57:41 -0600

    removed footer info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,13 @@ import './Footer.css'
 export default () => (
   <div>
     <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      {' '}
+      <a href=""></a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
-        <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
-        </span>
+
       </div>
     </footer>
   </div>
